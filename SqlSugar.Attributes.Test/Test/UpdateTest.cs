dc6dbad88173ec01abcfab6cbcb5f0ee00ab6e99
Willxup <wangxking@outlook.com>
2025-01-21 13:19:01 +0800

    create update test

---
+++ SqlSugar.Attributes.Test/Test/UpdateTest.cs
@@ -0,0 +1,26 @@
+﻿using SqlSugar.Attributes.Extension.Extensions;
+using SqlSugar.Attributes.Test.Entities;
+using SqlSugar.Attributes.Test.Models;
+
+namespace SqlSugar.Attributes.Test.Test
+{
+    public class UpdateTest
+    {
+        public static async Task Test()
+        {
+            SqlSugarScope db = DbContext.GetDb();
+
+            UpdateUserDto dto = new()
+            {
+                Id = 2,
+                Name = "Test5",
+                Email = null,
+                CreateTime = null
+            };
+
+            bool isSucess = db.Updateable<UpdateUserDto, User>(dto).ExecuteCommandHasChange();
+
+            Console.WriteLine($"Update Test1 Result = {isSucess}");
+        }
+    }
+}
