c6ec221d7f1f5e7942b9639730ec71045b4aae58
Willxup <wangxking@outlook.com>
2025-01-21 13:18:48 +0800

    create insert test

---
+++ SqlSugar.Attributes.Test/Models/UserDto.cs
@@ -0,0 +1,89 @@
+﻿using SqlSugar.Attributes.Extension.Common;
+using SqlSugar.Attributes.Extension.Extensions.Attributes.Operation;
+using SqlSugar.Attributes.Extension.Extensions.Attributes.Query;
+
+namespace SqlSugar.Attributes.Test.Models
+{
+
+    public class UserPageSearch : DbQueryModel
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [DbTableAlias("user")]
+        [DbQueryField("UserName")]
+        [DbQueryOperator(DbOperator.Like)]
+        public string Name { get; set; }
+        [DbTableAlias("user")]
+        [DbQueryField("Email")]
+        [DbQueryOperator(DbOperator.Like)]
+        public string Email { get; set; }
+    }
+    public class UserPageResult
+    {
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        [DbTableAlias("user")]
+        [DbQueryField("UserId")]
+        public long? Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [DbTableAlias("user")]
+        [DbQueryField("UserName")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [DbTableAlias("user")]
+        [DbQueryField("Email")]
+        public string Email { get; set; }
+        /// <summary>
+        /// 角色名
+        /// </summary>
+        [DbTableAlias("role")]
+        [DbQueryField("RoleName")]
+        public string RoleName { get; set; }
+    }
+
+    public class InsertUserDto
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [DbOperationField("UserName")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [DbOperationField("Email", false, true)]
+        public string Email { get; set; }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+    }
+    public class UpdateUserDto
+    {
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        [DbOperationField("UserId", true)]
+        public long? Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [DbOperationField("UserName")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [DbOperationField("Email", false ,true)]
+        public string Email { get; set; }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+    }
+}
---
+++ SqlSugar.Attributes.Test/Test/InsertTest.cs
@@ -0,0 +1,24 @@
+﻿using SqlSugar.Attributes.Extension.Extensions;
+using SqlSugar.Attributes.Test.Entities;
+using SqlSugar.Attributes.Test.Models;
+
+namespace SqlSugar.Attributes.Test.Test
+{
+    public class InsertTest
+    {
+        public static async Task Test()
+        {
+            var db = DbContext.GetDb();
+
+            InsertUserDto user = new()
+            {
+                Name = "Test5",
+                Email = "test5@willxup.top",
+                CreateTime = db.GetDate()
+            };
+
+            long id = await db.Insertable<InsertUserDto, User>(user).ExecuteReturnBigIdentityAsync();
+            Console.WriteLine(id);
+        }
+    }
+}
