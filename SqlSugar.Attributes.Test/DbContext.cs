64b400b3099fc75ff6c6a03f047986f29b59459e
Willxup <wangxking@outlook.com>
2025-01-21 13:18:07 +0800

    test project init

---
+++ SqlSugar.Attributes.Test/DbContext.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+
+namespace SqlSugar.Attributes.Test
+{
+    public class DbContext
+    {
+        private static bool _isInit = false;
+        private static SqlSugarScope _scope;
+        private readonly static string _connection = "server=127.0.0.1;Port=3306;Database=sa_test;Uid=root;Pwd=liugui.com;AllowLoadLocalInfile=true";
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private static void Init()
+        {
+            _scope = new SqlSugarScope(new ConnectionConfig()
+            {
+                ConnectionString = _connection,
+                DbType = DbType.MySql,
+                LanguageType = LanguageType.English,
+                IsAutoCloseConnection = true,
+            },
+            it => {
+                it.Aop.OnLogExecuting = (sql, parameters) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo));
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("【SQL】：");
+                    Console.ResetColor();
+                    Console.WriteLine(sql);
+                    if (parameters?.Length > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo));
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("【Paramters】：");
+                        Console.ResetColor();
+                        Console.WriteLine(string.Join(",", parameters.Select(it => "【" + it.ParameterName + "=" + it.Value + "】")));
+                    }
+                };
+            });
+
+            _isInit = true;
+        }
+
+        /// <summary>
+        /// 获取Db
+        /// </summary>
+        /// <returns></returns>
+        public static SqlSugarScope GetDb()
+        {
+            if(!_isInit)
+            {
+                Init();
+            }
+
+            return _scope;
+        }
+    }
+}
---
+++ SqlSugar.Attributes.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using SqlSugar.Attributes.Test.Test;
+
+namespace SqlSugar.Attributes.Test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            InitData.Init();
+
+            await QueryTest.Test();
+            await InsertTest.Test();
+            await UpdateTest.Test();
+
+            Console.ReadKey();
+        }
+    }
+}
